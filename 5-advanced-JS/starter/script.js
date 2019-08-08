5e47445f119d137caf29b51c1eed83a8d1afad84
Carlos M Garcia <cgarcia101015@gmail.com>
2019-08-07 23:07:00 -0400

    Objects and Functions

--- 5-advanced-JS/starter/script.js
+++ 5-advanced-JS/starter/script.js
@@ -10,12 +10,11 @@ var Person = function (name, yearOfBirth, job) {
     this.name = name;
     this.yearOfBirth = yearOfBirth;
     this.job = job;
-    this.calculateAge
 }
 
 Person.prototype.calculateAge = function () {
     console.log(2016 - this.yearOfBirth);
-};
+}
 
 Person.prototype.lastName = 'Smith';
 
@@ -30,9 +29,10 @@ mark.calculateAge();
 console.log(john.lastName);
 console.log(jane.lastName);
 console.log(mark.lastName);
+
 */
 
-/*
+
 
 // Object.create
 var personProto = {
@@ -46,399 +46,22 @@ john.name = 'John';
 john.yearOfBirth = 1990;
 john.job = 'teacher';
 
+
 var jane = Object.create(personProto, {
-    name: {
-        value: 'Jane'
-    },
-    yearOfBirth: {
-        value: 1969
-    },
-    job: {
-        value: 'designer'
-    }
-});
-*/
+    name: { value: 'Jane' },
+    year: { value: 1969 },
+    job: { value: 'designer' }
+})
 
-//Primtitives vs Objects
-/*
-// Primatives
-var a = 23;
-var b = a;
-a = 46;
-console.log(a);
-console.log(b);
 
-//Objects
-var obj1 = {
-    name: 'John',
-    age: 26
-};
-var obj2 = obj1;
-obj1.age = 30;
-console.log(obj1.age);
-console.log(obj2.age);
 
-// Functions
-var age = 27;
-var obj = {
-    name: 'Jonas',
-    city: 'Lisbon'
-};
 
-function change(a, b) {
-    a = 30;
-    b.city = 'San Francisco';
-}
 
-change(age, obj);
 
-console.log(age);
-console.log(obj.city);
 
-/////////////////////////////////
-// Lecture: Passing functions as arguments
 
-var years = [1990, 1965, 1937, 2005, 1998];
 
-function arrayCalc(arr, fn) {
-    var arrRes = [];
-    for (var i = 0; i < arr.length; i++) {
-        arrRes.push(fn(arr[i]));
-    }
-    return arrRes;
-};
 
-function calculateAge(el) {
-    return 2016 - el;
-}
 
-function isFullAge(el) {
-    return el >= 18;
-}
 
-function maxHeartRate(el) {
-    if (el >= 18 && el <= 81) {
-        return Math.round(206.9 - (0.67 * el));
-    } else {
-        return -1;
-    }
 
-}
-
-var ages = arrayCalc(years, calculateAge);
-var fullAges = arrayCalc(ages, isFullAge);
-var rates = arrayCalc(ages, maxHeartRate);
-console.log(ages);
-console.log(rates);*/
-
-//////////////////////////
-//Lecture: Functions returning functions
-
-
-
-// var teacherQuestion = interviewQuestion('teacher');
-// var designerQuestion = interviewQuestion('designer');
-// teacherQuestion('John');
-// designerQuestion('John');
-// designerQuestion('Jane');
-// designerQuestion('Mark');
-// designerQuestion('Mike');
-
-// interviewQuestion('teacher')('Mark');
-
-//////////////////////////
-// Lecture: IIFE
-
-// function game() {
-
-// }
-
-// game();
-
-// (function () {
-//     var score = Math.random() * 10;
-//     console.log(score >= 5);
-// })();
-
-// // console.log(score);
-
-// (function (goodLuck) {
-//     var score = Math.random() * 10;
-//     console.log(score >= 5 - goodLuck);
-// })(5);
-
-//////////////////////////
-// Lecture: Closures
-
-// function retirement(retirementAge) {
-//     var a = ' years left until retirement.';
-//     return function (yearOfBirth) {
-//         var age = 2016 - yearOfBirth;
-//         console.log((retirementAge - age) + a);
-//     }
-// }
-
-// var retirementUS = retirement(66);
-// var retirementGermany = retirement(65);
-// var retirementIceland = retirement(67);
-// retirementGermany(1990);
-// retirementUS(1990);
-// retirementIceland(1990);
-
-
-// retirement(66)(1990);
-/*
-function interviewQuestion(job) {
-
-    if (job === 'designer') {
-        return function (name) {
-            console.log(name + ', can you please explain what UX design is?');
-        }
-    } else if (job === 'teacher') {
-        return function (name) {
-            console.log('What subject do you teach, ' + name + '?');
-        }
-    } else {
-        return function (name) {
-            console.log('Hello ' + name + ', what do you do?');
-        }
-    }
-}
-*/
-
-
-
-// function interviewQuestion(job) {
-//     return function (name) {
-//         if (job === 'designer') {
-//             console.log(name + ', can you please explain what UX design is?');
-//         } else if (job === 'teacher') {
-//             console.log('What subject do you teach, ' + name + '?');
-//         } else {
-//             console.log('Hello ' + name + ', what do you do?');
-//         }
-//     }
-// }
-
-// interviewQuestion('teacher')('John');
-
-// ///////////////////////////
-// // Lecture: Bind, call and apply
-
-// var john = {
-//     name: 'John',
-//     age: 26,
-//     job: 'teacher',
-//     presentation: function (style, timeOfDay) {
-//         if (style === 'formal') {
-//             console.log('Good ' + timeOfDay + ' , Ladies and gentleman! I\'m ' +
-//                 this.name + ', I\'m a ' + this.job + ' and I\'m ' + this.age + ' years old.');
-//         } else if (style === 'friendly') {
-//             console.log('Hey What\'s up? I\'m ' +
-//                 this.name + ', I\'m a ' + this.job + ' and I\'m ' + this.age + ' years old. Have a nice ' + timeOfDay + '.');
-//         }
-//     }
-// }
-
-// var emily = {
-//     name: 'Emily',
-//     age: 35,
-//     job: 'designmer'
-// };
-// john.presentation('formal', 'morning');
-
-// john.presentation.call(emily, 'friendly', 'afternoon');
-
-// // john.presentation.apply(emily, ['friendly', 'afternoon']);
-
-// var johnFriendly = john.presentation.bind(john, 'friendly');
-
-// johnFriendly('morning');
-// johnFriendly('night');
-
-// var emilyFormal = john.presentation.bind(emily, 'formal');
-// emilyFormal('afternoon');
-
-/////////////////////////////
-// CODING CHALLENGE
-/*var question = {
-    question: 'Hi how are you?',
-    answers: ['Great and you?', 'I\'ve seen better days', 'I\m ok'],
-    correctAnswer: 0,
-
-}
-var Questions = function (question, answers, correctAnswer) {
-    this.question = question;
-    this.answers = answers;
-    this.correctAnswer = correctAnswer;
-    this.randomQuestion
-}
-Questions.prototype.randomQuestion = function () {
-    var randomQ = questionArray[Math.floor(Math.random() * 3)];
-    console.log(randomQ.question);
-    console.log('0: ' + randomQ.answers[0]);
-    console.log('1: ' + randomQ.answers[1]);
-    console.log('2: ' + randomQ.answers[2]);
-
-};
-
-var question1 = new Questions('Is JavaScript fun?', ['Hell yea', 'OMG No', 'Kinda Maybe Sorta'], 0);
-
-var question2 = new Questions('Are you hungry?', ['Yes', 'No', 'I could eat!'], 2);
-
-var question3 = new Questions('What language are you looking forward to learning?', ['React', 'Swift', 'Android'], 0);
-
-var questionArray = [question1, question2, question3];
-
-question1.randomQuestion();
-
-function myFunction() {
-    var person = prompt("Please enter the correct answer.");
-    console.log(person);
-    answerCheck();
-
-    function answerCheck() {
-        if (person === this.correctAnswer) {
-            console.log(person + ' is the correct answer!')
-        }
-    }
-}
-
-myFunction();
-// Function constructor
-/*
-var john = {
-    name: 'John',
-    yearOfBirth: 1990,
-    job: 'teacher'
-};
-
-var Person = function (name, yearOfBirth, job) {
-    this.name = name;
-    this.yearOfBirth = yearOfBirth;
-    this.job = job;
-    this.calculateAge
-}
-
-Person.prototype.calculateAge = function () {
-    console.log(2016 - this.yearOfBirth);
-};
-*/
-/*
-(function () {
-
-
-
-    function Question(question, answers, correct) {
-        this.question = question;
-        this.answers = answers;
-        this.correct = correct;
-    }
-
-    Question.prototype.displayQuestion = function () {
-        console.log(this.question);
-
-        for (var i = 0; i < this.answers.length; i++) {
-            console.log(i + ': ' + this.answers[i]);
-        };
-    };
-
-    Question.prototype.checkAnswer = function (ans) {
-        if (ans === this.correct) {
-            console.log('Correct answer!');
-        } else {
-            console.log('Wrong answer. Try again :)');
-        }
-    }
-
-    var q1 = new Question('Is JavaScript the coolest programming language in the world?', ['Yes', 'No'], 0);
-
-    var q2 = new Question('What is the name of this course\'s teacher?', ['John', 'Micheal', 'Jonas'], 2);
-
-    var q3 = new Question('What does best describe coding?', ['Boring', 'Hard', 'Fun', 'Tediuos'], 2);
-
-    var questions = [q1, q2, q3];
-
-    var n = Math.floor(Math.random() * questions.length);
-
-    questions[n].displayQuestion();
-
-    var answer = parseInt(prompt('Please select the correct answer.'));
-
-    questions[n].checkAnswer(answer);
-})();
-*/
-
-
-(function () {
-
-
-
-    function Question(question, answers, correct) {
-        this.question = question;
-        this.answers = answers;
-        this.correct = correct;
-    }
-
-    Question.prototype.displayQuestion = function () {
-        console.log(this.question);
-
-        for (var i = 0; i < this.answers.length; i++) {
-            console.log(i + ': ' + this.answers[i]);
-        };
-    };
-
-    Question.prototype.checkAnswer = function (ans, callback) {
-        var sc;
-        if (ans === this.correct) {
-            console.log('Correct answer!');
-            sc = callback(true);
-        } else {
-            console.log('Wrong answer. Try again :)');
-            sc = callback(false);
-        }
-        this.displayScore(sc);
-    }
-
-    Question.prototype.displayScore = function (score) {
-        console.log('Your current score is ' + score);
-        console.log('------------------------------------');
-    }
-
-    var q1 = new Question('Is JavaScript the coolest programming language in the world?', ['Yes', 'No'], 0);
-
-    var q2 = new Question('What is the name of this course\'s teacher?', ['John', 'Micheal', 'Jonas'], 2);
-
-    var q3 = new Question('What does best describe coding?', ['Boring', 'Hard', 'Fun', 'Tediuos'], 2);
-
-    var questions = [q1, q2, q3];
-
-    function score() {
-        var sc = 0;
-        return function (correct) {
-            if (correct) {
-                sc++;
-            }
-            return sc;
-        }
-    }
-
-    var keepScore = score();
-
-    function nextQuestion() {
-
-        var n = Math.floor(Math.random() * questions.length);
-
-        questions[n].displayQuestion();
-
-        var answer = prompt('Please select the correct answer.');
-
-        if (answer !== 'exit') {
-            questions[n].checkAnswer(parseInt(answer), keepScore);
-
-            nextQuestion();
-        }
-    }
-
-    nextQuestion();
-})();
