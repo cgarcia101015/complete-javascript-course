341f6b64c7ed7d234d27e2fee1e965840c96b390
Carlos M Garcia <cgarcia101015@gmail.com>
2019-08-16 23:43:21 -0400

    IIFE

--- 5-advanced-JS/starter/script.js
+++ 5-advanced-JS/starter/script.js
@@ -32,7 +32,7 @@ console.log(mark.lastName);
 
 */
 
-
+/*
 
 // Object.create
 var personProto = {
@@ -52,6 +52,165 @@ var jane = Object.create(personProto, {
     year: { value: 1969 },
     job: { value: 'designer' }
 })
+*/
+/*
+// Primitives and objects
+
+// Primitives
+var a = 23;
+var b = a;
+a = 46;
+console.log(a);
+console.log(b);
+
+
+// Objects
+var obj1 = {
+    name: 'John',
+    age: 26
+};
+
+var obj2 = obj1;
+obj1.age = 30;
+console.log(obj1.age);
+console.log(obj2.age);
+
+// Functions
+var age = 27;
+var obj = {
+    name: 'Jonas',
+    city: 'Lisbon'
+};
+
+function change(a, b) {
+    a = 30;
+    b.city = 'San Francisco';
+}
+change(age, obj);
+console.log(age);
+console.log(obj.city);
+*/
+
+/////////////////////////////////////
+/// Lecture: Passing functions as arguments
+/*
+var years = [1990, 1965, 1937, 2005, 1998];
+
+function arrayCalc(arr, fn) {
+    var arrRes = [];
+    for (var i = 0; i < arr.length; i++) {
+        arrRes.push(fn(arr[i]));
+    }
+    return arrRes;
+}
+
+function calculateAge(el) {
+    return 2016 - el;
+}
+
+function isFullAge(el) {
+    return el >= 18;
+}
+
+function maxHeartRate(el) {
+    if (el >= 18 && el <= 81) {
+        return Math.round(206.9 - (0.67 * el));
+    } else {
+        return -1;
+    }
+
+}
+
+
+var ages = arrayCalc(years, calculateAge);
+var fullAges = arrayCalc(ages, isFullAge);
+var rates = arrayCalc(ages, maxHeartRate);
+
+console.log(ages);
+console.log(rates);
+*/
+
+
+/////////////////////////////////////
+// Lecture: Functions returning functions
+/*
+function interviewQuestion(job) {
+    if (job === 'designer') {
+        return function (name) {
+            console.log(name + ', can you please explain what UX design is?');
+        }
+    } else if (job === 'teacher') {
+        return function (name) {
+            console.log('What subject do you teach, ' + name + '?');
+        }
+    } else {
+        return function (name) {
+            console.log('Hello ' + name + ', what do you do?');
+        }
+    }
+}
+
+var teacherQuestion = interviewQuestion('teacher');
+var designerQuestion = interviewQuestion('designer');
+teacherQuestion('John');
+designerQuestion('John');
+designerQuestion('Jane');
+designerQuestion('Mark');
+designerQuestion('Mike');
+
+interviewQuestion('teacher')('Mark');
+
+
+*/
+
+/////////////////////////////////
+// Lecture: IIFE
+
+// function game() {
+
+// }
+// game();
+
+(function () {
+    var score = Math.random() * 10;
+    console.log(score >= 5);
+})();
+// console.log(score);
+
+(function (goodLuck) {
+    var score = Math.random() * 10;
+    console.log(score >= 5 - goodLuck);
+})(5);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
